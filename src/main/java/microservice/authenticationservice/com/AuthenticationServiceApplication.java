4f0d66a40cb12cf86c443f075b6f87542bd0beaf
zikrirahman26 <zikrirahman014@gmail.com>
2025-02-10 17:49:22 +0700

    Update Eureka Authentication Service

--- src/main/java/microservice/authenticationservice/com/AuthenticationServiceApplication.java
+++ src/main/java/microservice/authenticationservice/com/AuthenticationServiceApplication.java
@@ -2,8 +2,10 @@ package microservice.authenticationservice.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
